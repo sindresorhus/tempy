95b4ce68b014f2656b8f46a40d06942b72cbf645
TiagoDanin <TiagoDanin@outlook.com>
2019-05-21 22:39:44 -0300

    Improve examples

--- index.d.ts
+++ index.d.ts
@@ -28,7 +28,6 @@ declare const tempy: {
 	@example
 	```
 	import tempy = require('tempy');
-	import pathExists = require('path-exists');
 
 	tempy.file();
 	//=> '/private/var/folders/3x/jf5977fn79jbglr7rk0tq4d00000gn/T/4f504b9edb5ba0e89451617bf9f971dd'
@@ -38,17 +37,6 @@ declare const tempy: {
 
 	tempy.file({name: 'unicorn.png'});
 	//=> '/private/var/folders/3x/jf5977fn79jbglr7rk0tq4d00000gn/T/f7f62bfd4e2a05f1589947647ed3f9ec/unicorn.png'
-
-	tempy.directory();
-	//=> '/private/var/folders/3x/jf5977fn79jbglr7rk0tq4d00000gn/T/2f3d094aec2cb1b93bb0f4cffce5ebd6'
-
-	tempy.clean();
-	//=> ['/private/var/folders/3x/jf5977fn79jbglr7rk0tq4d00000gn/T/4f504b9edb5ba0e89451617bf9f971dd', ...]
-
-	(async () => {
-		console.log(await tempy.job(directory => pathExists(directory)));
-		//=> true
-	})();
 	```
 	*/
 	file(options?: tempy.Options): string;
@@ -70,19 +58,46 @@ declare const tempy: {
 	Deletes temporary directories and returns an array of deleted path.
 
 	@returns An array of deleted paths.
+
+	@example
+	```
+	import tempy = require('tempy');
+
+	tempy.clean();
+	//=> ['/private/var/folders/3x/jf5977fn79jbglr7rk0tq4d00000gn/T/4f504b9edb5ba0e89451617bf9f971dd', ...]
+	```
 	*/
 	clean(): string[];
 
 	/**
 	Returns a `Promise` for value obtained in `task`.
 
-	@returns Task output
 	@param task - A function that will be called with a temporary directory path. The directory is created and deleted when `Promise` is resolved.
+	@returns Task output
+
+	@example
+	```
+	import pathExists = require('path-exists');
+	import tempy = require('tempy');
+
+	(async () => {
+		console.log(await tempy.job(directory => pathExists(directory)));
+		//=> true
+	})();
+	```
 	*/
 	job(task: (directory: string) => unknown): Promise<unknown>;
 
 	/**
-	Get the root temporary directory path. For example: `/private/var/folders/3x/jf5977fn79jbglr7rk0tq4d00000gn/T`.
+	Get the root temporary directory path.
+
+	@example
+	```
+	import tempy = require('tempy');
+
+	tempy.root;
+	//=> '/private/var/folders/3x/jf5977fn79jbglr7rk0tq4d00000gn/T'
+	```
 	*/
 	readonly root: string;
 };
