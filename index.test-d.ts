e2385528c18898b005f48607d1d38e9b4cc9ab97
Malik Ammar Faisal <30975614+ammarbinfaisal@users.noreply.github.com>
2019-07-05 21:05:23 +0530

    use top-level await

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,12 +1,14 @@
 import {expectType, expectError} from 'tsd';
 import tempy = require('.');
 
-const options: tempy.Options = {};
-expectType<string>(tempy.directory());
-expectType<string>(tempy.file());
-expectType<string>(tempy.file({extension: 'png'}));
-expectType<string>(tempy.file({name: 'afile.txt'}));
-expectError(tempy.file({extension: 'png', name: 'afile.txt'}));
-expectType<string>(tempy.root);
-expectType<string>(tempy.writeSync('hi there'));
-(async () => {expectType<string>(await tempy.write('hi there'))});
+(async () => {
+    const options: tempy.Options = {};
+    expectType<string>(tempy.directory());
+    expectType<string>(tempy.file());
+    expectType<string>(tempy.file({extension: 'png'}));
+    expectType<string>(tempy.file({name: 'afile.txt'}));
+    expectError(tempy.file({extension: 'png', name: 'afile.txt'}));
+    expectType<string>(tempy.root);
+    expectType<string>(tempy.writeSync('hi there'));
+    expectType<string>(await tempy.write('hi there'));
+})
