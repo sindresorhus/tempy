2a35e7c1e17437f0d9e00860f67f3b0683f5a02e
Malik Ammar Faisal <30975614+ammarbinfaisal@users.noreply.github.com>
2019-07-06 14:28:23 +0530

    remove async function

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,16 +1,14 @@
 import {expectType, expectError} from 'tsd';
 import tempy = require('.');
 
-(async () => {
-    const options: tempy.Options = {};
-    expectType<string>(tempy.directory());
-    expectType<string>(tempy.file());
-    expectType<string>(tempy.file({extension: 'png'}));
-    expectType<string>(tempy.file({name: 'afile.txt'}));
-    expectError(tempy.file({extension: 'png', name: 'afile.txt'}));
-    expectError(tempy.file({name: 'file.txt', filePath: '/blah/blah/blah/file.txt'}));
-    expectType<string>(tempy.root);
-    expectType<string>(tempy.writeSync('hi there'));
-    expectType<string>(await tempy.write('hi there'));
-    expectType<boolean>(tempy.exists('any/directory'));
-})
+const options: tempy.Options = {};
+expectType<string>(tempy.directory());
+expectType<string>(tempy.file());
+expectType<string>(tempy.file({extension: 'png'}));
+expectType<string>(tempy.file({name: 'afile.txt'}));
+expectError(tempy.file({extension: 'png', name: 'afile.txt'}));
+expectError(tempy.file({name: 'file.txt', filePath: '/blah/blah/blah/file.txt'}));
+expectType<string>(tempy.root);
+expectType<string>(tempy.writeSync('hi there'));
+expectType<string>(await tempy.write('hi there'));
+expectType<boolean>(tempy.exists('any/directory'));
