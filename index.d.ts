95b265947b395b07ad3be06f14e3cf096e6c1cc3
Sindre Sorhus <sindresorhus@gmail.com>
2020-03-07 16:30:59 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -75,7 +75,7 @@ declare const tempy: {
 	//=> '/private/var/folders/3x/jf5977fn79jbglr7rk0tq4d00000gn/T/2f3d094aec2cb1b93bb0f4cffce5ebd6'
 	```
 	*/
-	write(fileContent: tempy.WritableType | NodeJS.ReadableStream, options?: tempy.Options): Promise<string>
+	write(fileContent: tempy.WritableType | NodeJS.ReadableStream, options?: tempy.Options): Promise<string>;
 
 	/**
 	Synchronously write a string/buffer to a random temp file.
@@ -88,7 +88,7 @@ declare const tempy: {
 	//=> '/private/var/folders/3x/jf5977fn79jbglr7rk0tq4d00000gn/T/2f3d094aec2cb1b93bb0f4cffce5ebd6'
 	```
 	*/
-	writeSync(fileContent: tempy.WritableType, options?: tempy.Options): string
+	writeSync(fileContent: tempy.WritableType, options?: tempy.Options): string;
 
 	/**
 	Get the root temporary directory path.
